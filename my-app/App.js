ee7916b364f66aa9154efe44b6bdb2ef3b4309fd
unknown <2222130059@ACADEMICO.IESB.BR>
2025-03-28 09:13:26 -0300

    att

--- my-app/App.js
+++ my-app/App.js
@@ -6,15 +6,18 @@ export default function App()
 {
   return (
     <View style={styles.main_container}>
-    <View style={{width:'60%', position:'absolute', left:5, top:30}}>
-      <TextInput style={styles.inputText} placeholder={rotulo_input_meta}/>
-    </View>
+    <View style={{flexDirection: 'row', justifyContent: 'space-between', flex: 1}}>
+     <View style = {{width: '65%'}}>
+        <TextInput style={styles.inputText} placeholder={rotulo_input_meta}/>
+      </View>
     
-    <View style={{width:'40%', position:'absolute', right:5, top:30}}>
-      <button title={rotulo_btn_cadastro_meta}/>
+      <View style = {{width: '30%'}}>
+        <button title={rotulo_btn_cadastro_meta}/>
+      </View>
+
     </View> 
 
-    <View style={{width:150, position:'absolute', left:5, top:80}}>
+    <View style = {styles.metaConteiner}>
       <text> const rotulo_lista_metas </text>
     </View>
     </View>
@@ -24,19 +27,23 @@ export default function App()
 const styles = StyleSheet.create({
   container: 
   {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
   main_container:
   {
     padding : 30,
+    flex: 1,
+    flexDirection: 'column'
   },
   inputText:
   {
     borderColor : '#ccccccc',
     borderWidth : 1,
+  },
+  metaConteiner:
+  {
+    flex: 1,
   }
 });
 
